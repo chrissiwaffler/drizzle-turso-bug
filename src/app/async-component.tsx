d3c8b215e91abafed12ad7124194986c8650cc8e
Christoph Waffler <waffler.christoph@gmail.com>
2023-09-14 01:49:43 +0200

    feat: add full client side

--- src/app/async-component.tsx
+++
@@ -1,9 +0,0 @@
-import { getDashboardData } from "@/lib/db/dashboard";
-
-export default async function AsyncComponent() {
-  const data = await getDashboardData();
-
-  if (data) {
-    return <div> Data is successfully loaded: {data[0].apy} </div>;
-  }
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
 "use client";
 
-import { Suspense } from "react";
-import AsyncComponent from "./async-component";
+import { getDashboardData } from "@/lib/db/dashboard";
+import { useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState<{ apy: number; tvl: number }[]>([]);
+
+  if (!data) {
+    getDashboardData().then((data) => {
+      setData(data);
+    });
+  }
+
+  if (data) {
+    return <div> Data is successfully loaded: {data[0].apy} </div>;
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Suspense fallback={<div>loading...</div>}>
-        <AsyncComponent />
-      </Suspense>
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
 }
